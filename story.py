ddc263773c840eeebf99c3b61ae2439240360319
charlie-wt <cwt1g14@soton.ac.uk>
2017-06-20 17:44:28 +0100

    Updated the story JSON importer to be a little closer to complete.

--- story.py
+++ story.py
@@ -20,16 +20,82 @@ class Story:
         json_object = json.loads(data)
         if prnt[1]: print(json_object)
         
+        # id
+        self.id = json_object["id"]
+        
+        # name
+        self.name = json_object["name"]
+        
+        # pages
+        for page in json_object["pages"]:
+            self.pages.append(Page(
+                page["id"],
+                page["name"],
+                page["functions"],
+                page["conditions"]))
+        
+        # conditions
+        for condition in json_object["conditions"]:
+            cond = Condition(condition["id"], condition["type"])
+            if ( "bool" in condition ): cond.bool = condition["bool"]
+            if ( "location" in condition ): cond.location = condition["location"]
+            if ( "operand" in condition ): cond.operand = condition["operand"]
+            if ( "a" in condition ): cond.a = condition["a"]
+            if ( "aType" in condition ): cond.aType = condition["aType"]
+            if ( "b" in condition ): cond.b = condition["b"]
+            if ( "bType" in condition ): cond.bType = condition["bType"]
+            self.conditions.append(cond)
+        
+        # functions
+        for function in json_object["functions"]:
+            self.functions.append(Function(
+                function["id"],
+                function["type"],
+                function["conditions"],
+                function["variable"],
+                function["value"]))
+
         # locations
         for location in json_object["locations"]:
             self.locations.append(Location(
                 location["id"],
                 location["type"],
-                location["lat"],
-                location["lon"],
-                location["radius"]))
+                float(location["lat"]),
+                float(location["lon"]),
+                float(location["radius"])))
             if prnt[2]: print(self.locations[len(self.locations)-1].id)
+        
+        if prnt[3]: print("'", self.name, "' is story", self.id, "and contains", len(self.pages), "pages,", len(self.conditions), "conditions,", len(self.functions), "functions &", len(self.locations), "locations.")
 
+class Page:
+    def __init__ ( self, id, name, functions, conditions ):
+        self.id = id
+        self.name = name
+        self.functions = functions
+        self.conditions = conditions
+
+class Function:
+    # TODO: different types of functions' attributes, execution etc.
+    def __init__ ( self, id, type, conditions, variable, value ):
+        self.id = id
+        self.type = type
+        self.conditions = conditions
+        self.variable = variable
+        self.value = value
+
+class Condition:
+    # TODO: evaluation
+    def __init__ ( self, id, type ):
+        self.id = id
+        self.type = type
+        self.bool = None
+        self.location = None
+        self.operand = None
+        self.a = None
+        self.aType = None
+        self.b = None
+        self.bType = None
+    
 class Location:
     def __init__ ( self, id, type, lat, lon, radius ):
         self.id = id
@@ -39,4 +105,4 @@ class Location:
         self.radius = radius
 
 st = Story()
-st.fromJSON([False, False, True], "Naseem Formatted")
+st.fromJSON([False, False, False, True], "Naseem Formatted")
